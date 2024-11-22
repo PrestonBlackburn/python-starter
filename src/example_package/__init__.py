fae0a3afa9eccc157f155a132f707469f940adb0
PrestonBlackburn <prestonblckbrn@gmail.com>
2024-11-22 07:59:38 -0700

    first commit

---
+++ src/example_package/__init__.py
@@ -0,0 +1,5 @@
+__all__ = [
+    "example_function"
+]
+
+from example_package.example import example_function
---
+++ tests/test_example_function.py
@@ -0,0 +1,6 @@
+from example_package import example_function
+
+
+def test_example_function():
+    example_function()
+    assert "your test" == "your test"
